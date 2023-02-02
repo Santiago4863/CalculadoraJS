8ae1c9613cc9a41a91f11e1a469411c8386fdfc8
Santiago González <santiagogonzalez4863@gmail.com>
2023-02-02 15:55:25 -0300

    Calculadora

--- javascript/app.js
+++ javascript/app.js
@@ -3,6 +3,7 @@ const displayValorActual = document.getElementById('valorActual');
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
 
+
 const display = new Display (displayValorAnterior, displayValorActual);
 
 botonesNumeros.forEach(boton =>{
@@ -11,4 +12,10 @@ botonesNumeros.forEach(boton =>{
 
 botonesOperadores.forEach(boton =>{
     boton.addEventListener('click', () => display.computar(boton.value))
+});
+
+getAllPosts().then(response => {
+    console.log(response);
+}).catch(e => {
+    console.log(e);
 });
